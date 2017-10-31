433fd10f029469cd8d3eae11ae371798e76c7343
chris bailey <cmbailey87@live.com>
2017-10-30 23:18:47 -0400

    reverted app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 //add the over lay
 //$variablename for jquery objects
 
-var $overlay = $('<div id="overlay"></div>');
+var $overlaycovers = $('<div id="overlaycovers"></div>');
 //disembodied element from the DOM
 var $image = $("<img>");
 //another diembodied element for caoption
@@ -11,16 +11,16 @@ var $caption = $("<p></p>");
 // caption to acompany image overlay
 
 //image is added to the overlay DIV disembodied element
-$overlay.append($image);
+$overlaycovers.append($image);
 //append caption to overlay
-$overlay.append($caption);
+$overlaycovers.append($caption);
 
 //overlay object is added to the DOM here
-$("body").append($overlay);
+$("body").append($overlaycovers);
 
 //user clicks on link, around the image
 //1.capture click event on image ***created ID in the top UL element named imageGallery to reference all child anchors
-$("#anthcovers a").click(function(event) {
+$("#anthcovers a").click(function() {
     event.preventDefault();
     //*this refers to the image anchor you clicked on
     var imagelocation = $(this).attr("href");
@@ -29,7 +29,7 @@ $("#anthcovers a").click(function(event) {
 
 
     //1.1show overlay
-    $overlay.show();
+    $overlaycovers.show();
     // console.log(href);
 
 
@@ -46,7 +46,7 @@ $("#anthcovers a").click(function(event) {
 
 //3 user action to leave over lay, click overlay
 
-$overlay.click(function() {
+$overlaycovers.click(function() {
     $(this).hide();
 
 })
@@ -54,3 +54,9 @@ $overlay.click(function() {
 //3.1 hide overlay
 
 // end light box
+
+
+
+// 
+
+//
